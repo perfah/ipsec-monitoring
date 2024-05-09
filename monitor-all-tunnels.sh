97606b6920422135735353085ae64d67fbe003e4
Per Fahlander <perfah@kth.se>
2024-05-09 14:56:41 +0200

    Accommodate previous commit in monitor-all-tunnels

--- monitor-all-tunnels.sh
+++ monitor-all-tunnels.sh
@@ -5,5 +5,5 @@ tunnel_names=$(ipsec status | ack -o '(?<=^000 ")[^"/]*' | sort | uniq)
 echo "** Monitoring check of all tunnels at $(date)"
 
 while IFS= read -r tunnel_name; do
-    /opt/ipsec-monitoring/monitor-tunnel.sh "$tunnel_name"
+    /opt/ipsec-monitoring/monitor-tunnel.sh "$tunnel_name" "$2"
 done <<< "$tunnel_names"
